54b1be7f57882efa4c63a794745e0578272c2059
Christian Osorio <ccdosorio11@gmail.com>
2022-09-07 00:55:31 -0600

    Empezando

--- modules/module1.js
+++
@@ -1,9 +0,0 @@
-const module = {
-    name: "module 1"
-}
-
-export function load() {
-    console.log(`${module.name} load!`);
-}
-
-export default module;
--- modules/module2.js
+++
@@ -1,9 +0,0 @@
-const module = {
-  name: "module 2",
-};
-
-export function load() {
-  console.log(`${module.name} load!`);
-}
-
-export default module;
