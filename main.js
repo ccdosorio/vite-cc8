20736360d2a6c8e26492260dd793cd020e88efba
Christian Osorio <ccdosorio11@gmail.com>
2022-09-07 00:11:05 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import { setupCounter } from './counter.js'
+import buttonStyles from './button.module.css';
+import imageStyles from './image.module.css';
+import img from './Spidey.jpeg';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <h2>test</h2>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+    <h3 id="btn">hola</h3>
+
+    <img id="img"></img>
+  </div>
+`
+
+document.getElementById('btn').className = buttonStyles.btn;
+const imagen = document.getElementById('img');
+imagen.src = img;
+imagen.className = imageStyles.img;
+
+
+setupCounter(document.querySelector('#counter'))
