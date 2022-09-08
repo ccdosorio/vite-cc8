b6b782c830a5035a47682e7b83b0bb83336277ca
Christian Osorio <ccdosorio11@gmail.com>
2022-09-08 15:46:02 -0600

    iniciar carousel

--- modules/module1.js
+++ modules/module1.js
@@ -1,23 +1,9 @@
 const module = {
-    name: "module 1"
+    name: "module 1 jquery"
 }
 
-window.addEventListener('mouseover', initLandbot, { once: true });
-window.addEventListener('touchstart', initLandbot, { once: true });
-var myLandbot;
-export function initLandbot() {
-    console.log('execute');
-    if (!myLandbot) {
-        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
-        s.addEventListener('load', function () {
-            var myLandbot = new Landbot.Livechat({
-                configUrl: 'https://storage.googleapis.com/landbot.site/v3/H-1286877-5NSS8NJMHL2HJUPC/index.json',
-            });
-        });
-        s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.js';
-        var x = document.getElementsByTagName('script')[0];
-        x.parentNode.insertBefore(s, x);
-    }
+export function load() {
+    console.log(module.name);
 }
 
 export default module;
